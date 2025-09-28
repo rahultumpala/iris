b0e8bf63a65fae13975b4fa89d07ee5a275a0ae2
rahultumpala <rhltumpala@gmail.com>
2025-09-28 19:38:47 +0530

    Generate static assets correctly

--- lib/iris.ex
+++ lib/iris.ex
@@ -1,26 +1,31 @@
 defmodule Iris do
   alias Iris.Core
 
+  defmacrop list_static() do
+    "iris/*"
+    |> Path.wildcard()
+    |> Enum.map(fn path ->
+      Module.put_attribute(__CALLER__.module, :external_resource, path)
+      {path, File.read!(path)}
+    end)
+  end
+
   def build(config) do
     core_entity = Core.build(config)
     {:ok, json} = Jason.encode(core_entity, [{:escape, :unicode_safe}, {:pretty, true}])
-    entity_content = "const getGlobalEntity = () => { return #{json}; }"
 
-    cwd_iris_path = "iris" |> Path.relative_to_cwd()
-    File.mkdir_p!(cwd_iris_path)
-    File.cp_r!("iris/", cwd_iris_path)
+    entity_content = "const getGlobalEntity = () => { return #{json}; }"
+    entity_path = "iris/entity.js"
+    entity_asset = {entity_path, entity_content}
 
     Mix.shell().info("Writing to iris/")
-    path = "iris/entity.js"
-    File.write!(path, entity_content)
 
-    copy_html()
-  end
+    [entity_asset | list_static()]
+    |> Enum.each(fn {path, content} ->
+      Mix.shell().info("Writing #{path}")
+      File.write!(path, content)
+    end)
 
-  defp copy_html do
-    "iris"
-    |> Path.relative_to_cwd()
-    |> File.ls()
-    |> IO.inspect()
+    Mix.shell().info("Iris view available at iris/index.html")
   end
 end
